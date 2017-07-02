d06fad79f1f7b03b1b6e2f04731e5afcc5f89f80
Valerie Leonie <vleonie.vl@gmail.com>
2017-07-02 23:34:15 +0700

    database

---
+++ src/util/DbConn.java
@@ -0,0 +1,10 @@
+package util;
+
+public class DbConn {
+
+    public static final String JDBC_CLASS = "com.mysql.jdbc.Driver";
+    public static final String JDBC_URL = "jdbc:mysql://localhost:3306/akuntansi";
+    public static final String JDBC_USERNAME = "root";
+    public static final String JDBC_PASSWORD = "root";
+
+}
